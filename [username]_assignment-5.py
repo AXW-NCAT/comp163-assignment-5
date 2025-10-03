97439b1a4640a8592ac01524f372464bf1416ee0
Aaron Williams <axwabq423@gmail.com>
2025-10-03 13:28:46 -0400

    Challenge 1: Collatz sequence- while loop for unknown iterations

--- [username]_assignment-5.py
+++ [username]_assignment-5.py
@@ -1,6 +1,14 @@
-n > 0
-if n % 2 == 0:
-  n / 2
-else:
-  (n * 3) + 1
-n += 1
+print("=== challenge 1: Collatz Conjecture ===")
+n = int(input(f"Enter starting number: "))
+print(f"Sequence: {n}", end=" ")
+count = 0
+
+while n != 1:
+    if n % 2 == 0:
+        n /= 2
+    else:
+        n = (n * 3) + 1
+    print(int(n),end=" ")
+    count +=1
+print()
+print(f"Steps: {count} \n")
